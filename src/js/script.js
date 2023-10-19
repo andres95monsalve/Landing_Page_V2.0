b3f5dc49644ee7df9bcde35fe58c28331a456007
Melvin Rivas <andres95monsalve@gmail.com>
2023-10-18 23:21:49 -0400

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -175,3 +175,45 @@ function showMenu() {
     }
   });
 }
+
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.imagen-final',
+    start: '-50% left',
+    end: '200% center',
+    scrub: true,
+    markers: false,
+    toggleActions: 'play reverse play reverse',
+  }
+});
+
+tl.to('.imagen-final', {
+  x: 800,
+  duration: 0.5
+});
+
+document.getElementById("start-page").addEventListener("click", scrollUp);
+
+function scrollUp(){
+
+    var currentScroll = document.documentElement.scrollTop;
+
+    if (currentScroll > 0){
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo (0, currentScroll - (currentScroll / 10));
+    }
+}
+
+buttonUp = document.getElementById("start-page");
+
+window.onscroll = function(){
+
+    var scroll = document.documentElement.scrollTop;
+
+    if (scroll > 500){
+        buttonUp.style.transform = "scale(1)";
+    }else if(scroll < 500){
+        buttonUp.style.transform = "scale(0)";
+    }
+
+}
